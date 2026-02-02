2a989f97acb80b5c220b2476a0f808b5c0338c12
André ZM <zulatonet@gmail.com>
2026-02-01 23:30:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // ← Adicione isso aqui!
   server: {
     host: true,
     port: 3000
